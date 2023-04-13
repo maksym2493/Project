e5e80a32dfb293d527555e12acf2214c963cea1e
maksym <24x9x3@gmail,com>
2023-04-13 19:30:30 +0300

    1

--- app/src/main/java/com/example/project/presentetion/main/MainRecipeAdapter.kt
+++ app/src/main/java/com/example/project/presentetion/main/MainRecipeAdapter.kt
@@ -1,16 +1,13 @@
 package com.example.project.presentetion.main
 
-import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.project.R
-import com.example.project.data.local.model.Recipe
 import com.example.project.databinding.RecyclerRowBinding
 import com.example.project.domain.main.model.MainRecipe
 
